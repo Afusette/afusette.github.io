7276c532df4dd2d66a78657da64fa48be61ba912
Océane W <oceane.wanecque@gmail.com>
2019-03-21 22:55:38 +0100

    add font

--- seconde page/page2.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.item');
-const thing = document.querySelector('.thing');
-
-
-items.forEach(item => item.addEventListener('mouseover', (e) => {
-  const {offsetLeft: left, offsetTop: top} = item;
-  thing.style.left = `${left}px`;
-}));
